6f92663f7a36589507cac57c8d24cfd8adbce7b1
FDU-donglab <157959771+FDU-donglab@users.noreply.github.com>
2025-07-01 15:41:06 +0800

    Update Test_GUI.py

--- Test_GUI.py
+++ Test_GUI.py
@@ -2,7 +2,8 @@ import datetime
 import logging
 import sys
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+# os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import json
 import time
 from collections import deque
 
@@ -19,36 +20,37 @@ from torch.utils.data import DataLoader
 
 from datasets.dataset import ReadDatasets
 from models.Unet_Lite import Unet_Lite
-from utils.config import *
-
+from utils.config import *  # Import json2args function and Args class
+# Please replace the import according to your project
+from test_in_gui import goTesting  # Import training function from your project if needed
 
 
 class EmittingStream(QObject):
-    textWritten = pyqtSignal(str)
+    textWritten = pyqtSignal(str)  # Signal for writing text
 
     def write(self, text):
-        self.textWritten.emit(str(text))
+        self.textWritten.emit(str(text))  # Emit the signal
 
 
 class TestingThread(QThread):
-    finished = pyqtSignal()
-    imageReady = pyqtSignal(np.ndarray, str)
+    finished = pyqtSignal()  # Signal emitted after testing is finished
+    imageReady = pyqtSignal(np.ndarray, str)  # Signal for sending images
 
     def __init__(self, parameters, parent = None):
         super(TestingThread, self).__init__(parent)
-        self.is_running = True
+        self.is_running = True  # Flag to indicate if testing is running
         self.parameters = parameters
 
     def run(self):
         if self.is_running:
-            frames_to_accumulate = 100
-            accumulated_raw_images = deque(maxlen = frames_to_accumulate)
-            accumulated_denoised_images = deque(maxlen = frames_to_accumulate)
+            frames_to_accumulate = 200  # Number of frames you want to accumulate
+            accumulated_raw_images = deque(maxlen = frames_to_accumulate)  # For accumulating raw frames
+            accumulated_denoised_images = deque(maxlen = frames_to_accumulate)  # For accumulating denoised frames
             args = self.parameters.args
             logging.info('Testing Start!!!')
             num_gpu = (len(args.gpu_ids.split(",")) + 1) // 2
             inputFileNames = list(os.walk(args.test_path, topdown = False))[-1][-1]
-            filename = 'models_' + os.path.basename(args.test_folder)
+            filename = f'models_{os.path.basename(args.train_folder)}'
             testSave_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'result',
                                         filename)
             print('testSave_dir = {}'.format(testSave_dir))
@@ -81,58 +83,46 @@ class TestingThread(QThread):
                     miniBatch = args.miniBatch_size
                     timeFrame = t
                     assert miniBatch <= timeFrame, "miniBatch size must <= time frame length !!!"
-                    for j in range(0, timeFrame, miniBatch):  # sub-batch
-                        if j + miniBatch <= timeFrame:
+                    for j in range(0, timeFrame, 1):  # sub-batch
+                        if j + miniBatch <= timeFrame:  # Ensure index does not go out of range
                             input_batch = input[:, j:j + miniBatch, ...]
                             output_p = model(input_batch).cpu().detach()
                             if j == 0:
-                                output[:, j:j + output_channels, ...] = output_p  # + input_batch_mean
+                                output[:, j:j + output_channels, ...] = output_p
                             else:
                                 output[:, j + output_channels - 1, ...] = output_p[:, -1, ...]
                                 output[:, j:j + output_channels - 1, ...] = 0.5 * (
                                         output_p[:, 0:-1, ...] + output[:, j:j + output_channels - 1, ...])
 
+                            # When enough frames have been accumulated, directly take these frames from output and send them
+                            if (j + 1) % frames_to_accumulate == 0 or j + 1 == timeFrame:
+                                # Calculate the start and end indices of the images to be sent
+                                start_idx = max(0, j + 1 - frames_to_accumulate)
+                                end_idx = j + 1
 
-                            if (j) % frames_to_accumulate == 0 or j >= timeFrame:
-
-                                start_idx = max(0, j - frames_to_accumulate)
-                                end_idx = min(j, timeFrame)
-
-
+                                # Directly extract corresponding frames from numpy array
                                 denoised_images = (output + mean).squeeze()[start_idx:end_idx, ...].numpy().astype(
                                     np.uint16)
                                 raw_images = (input + mean).squeeze().cpu().detach()[start_idx:end_idx,
                                              ...].numpy().astype(np.uint16)
 
-
-                                for idx in range(denoised_images.shape[0]):
+                                # Send accumulated images
+                                for idx in range(denoised_images.shape[0]):  # Iterate all accumulated frames
                                     self.imageReady.emit(raw_images[idx, ...], 'raw')
                                     self.imageReady.emit(denoised_images[idx, ...], 'denoised')
 
-
-                        if timeFrame % frames_to_accumulate != 0:
-                            remaining_frames = timeFrame % frames_to_accumulate
-                            denoised_images = (output + mean).squeeze()[:, -remaining_frames:, ...].numpy().astype(
-                                np.uint16)
-                            raw_images = (input + mean).squeeze().cpu().detach()[:, -remaining_frames:,
-                                         ...].numpy().astype(np.uint16)
-
-                            for idx in range(remaining_frames):
-                                self.imageReady.emit(raw_images[idx, ...], 'raw')
-                                self.imageReady.emit(denoised_images[idx, ...], 'denoised')
-
-                        accumulated_raw_images.clear()
-                        accumulated_denoised_images.clear()
+                    # Clear accumulated lists
+                    accumulated_raw_images.clear()
+                    accumulated_denoised_images.clear()
 
                     end = time.perf_counter()
-                    print('Processing time(s): ', end - start)
+                    print('Total processing time(s): ', end - start)
                     print('FPS:', t / (end - start))
-                    output = output[:, 0:t, ::]
+                    output = output[:, 0:t, ...]
                     output = output + mean
                     output_image = np.squeeze(output.numpy()) * 1.0
                     result_name = os.path.join(testSave_dir,
                                                datetime.datetime.now().strftime("%Y%m%d%H%M") + '_' + inputFileNames[i])
-                    print(result_name)
                     print(result_name)
                     output_image = np.clip(output_image, 0, 65535)
                     io.imsave(result_name, output_image.astype(np.uint16), check_contrast = False)
@@ -144,7 +134,7 @@ class TestingThread(QThread):
             self.finished.emit()
 
     def stop(self):
-
+        # Set the flag to indicate testing is stopped
         self.is_running = False
         print("\nTesting stopped")
         self.quit()
@@ -155,13 +145,13 @@ class TestingGUI(QMainWindow):
         super().__init__()
         self.redirect_stdout()
         self.initUI()
-        self.raw_image_queue = []
-        self.denoised_image_queue = []
-        self.display_timer = QtCore.QTimer(self)
-        self.display_timer.setInterval(1000 / 30)
-        self.display_timer.timeout.connect(self.display_next_image)
-        self.display_timer.start()
-
+        self.raw_image_queue = []  # For storing raw images
+        self.denoised_image_queue = []  # For storing denoised images
+        self.display_timer = QtCore.QTimer(self)  # Create a new timer
+        self.display_timer.setInterval(33)  # Set frame rate, e.g. 30fps, 33.33ms per frame
+        self.display_timer.timeout.connect(self.display_next_image)  # Connect timeout signal to image display slot
+        self.display_timer.start()  # Start timer
+        # Create and set queue info label
         self.queue_info_label = QLabel("Queue: 0")
         self.central_widget.layout().addWidget(self.queue_info_label)
 
@@ -169,7 +159,7 @@ class TestingGUI(QMainWindow):
         self.setWindowTitle("Testing GUI")
         self.setGeometry(100, 100, 700, 600)
 
-
+        # Set global stylesheet: background black, text white
         self.setStyleSheet("""
             QMainWindow {
                 background-color: black;
@@ -193,22 +183,22 @@ class TestingGUI(QMainWindow):
             }
         """)
 
-
+        # Create central widget and layout
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         main_layout = QHBoxLayout(self.central_widget)
 
-
+        # Create left layout
         left_layout = QVBoxLayout()
 
-
+        # Add Logo to the center of the left layout
         self.logo_label = QLabel()
-        logo_pixmap = QPixmap(r'./FAST_logo.png')
+        logo_pixmap = QPixmap(r'./FAST_logo.png')  # Replace with your logo path
         self.logo_label.setPixmap(logo_pixmap)
         self.logo_label.setAlignment(QtCore.Qt.AlignCenter)
-        left_layout.addWidget(self.logo_label)
+        left_layout.addWidget(self.logo_label)  # Add Logo to left layout center
 
-
+        # Create path and button layout
         path_and_buttons_layout = QVBoxLayout()
         self.json_path_label = QLabel("No model_json Path selected")
         path_and_buttons_layout.addWidget(self.json_path_label)
@@ -228,21 +218,21 @@ class TestingGUI(QMainWindow):
         self.stop_testing_btn = QPushButton('Stop testing')
         path_and_buttons_layout.addWidget(self.stop_testing_btn)
 
-
+        # Add path and button layout to left layout
         left_layout.addLayout(path_and_buttons_layout)
 
-
+        # Create log text editor
         self.log_text_edit = QTextEdit()
         left_layout.addWidget(self.log_text_edit)
 
-
+        # Create right layout
         self.raw_text_label = QLabel("Raw image:")
         self.denoised_text_label = QLabel("Denoised image:")
         right_layout = QVBoxLayout()
         self.image_label_raw = QLabel()
         self.image_label_denoised = QLabel()
 
-
+        # Set default image (black) for image labels
         for label in [self.image_label_raw, self.image_label_denoised]:
             label.setFixedSize(400, 400)
             label.setAlignment(QtCore.Qt.AlignCenter)
@@ -250,17 +240,17 @@ class TestingGUI(QMainWindow):
             black_image.fill(QtCore.Qt.black)
             label.setPixmap(black_image)
 
-
+        # Add widgets to right layout
         right_layout.addWidget(self.raw_text_label)
         right_layout.addWidget(self.image_label_raw)
         right_layout.addWidget(self.denoised_text_label)
         right_layout.addWidget(self.image_label_denoised)
 
-
+        # Add left and right layouts to main layout
         main_layout.addLayout(left_layout, 1)
         main_layout.addLayout(right_layout, 3)
 
-
+        # Set action connections
         self.select_folder_btn.clicked.connect(self.select_folder)
         self.select_json_btn.clicked.connect(self.select_json)
         self.start_testing_btn.clicked.connect(self.start_testing)
@@ -270,26 +260,26 @@ class TestingGUI(QMainWindow):
 
 
     def update_image(self, image_array, figure = 'denoised'):
-
+        # Add processed image to the corresponding queue according to its type
         if figure == 'raw':
             self.raw_image_queue.append(image_array)
         elif figure == 'denoised':
             self.denoised_image_queue.append(image_array)
 
     def display_next_image(self):
-
+        # Get and display images from raw and denoised image queues
         if self.raw_image_queue:
             raw_image = self.raw_image_queue.pop(0)
             self.display_image(raw_image, self.image_label_raw)
         if self.denoised_image_queue:
             denoised_image = self.denoised_image_queue.pop(0)
             self.display_image(denoised_image, self.image_label_denoised)
-
-        queue_length = len(self.raw_image_queue)
+        # Update queue info label
+        queue_length = len(self.raw_image_queue)  # Assume you care about the raw image queue
         self.queue_info_label.setText(f"Queue: {queue_length}")
 
     def display_image(self, image_array, image_label):
-
+        # Convert image and display on the specified QLabel
         image_array = normalize(image_array, pmin = 3, pmax = 99.8, clip = True) * 255.0
         image_array = image_array.astype(np.uint8)
         image = QImage(image_array.data, image_array.shape[1], image_array.shape[0], QImage.Format_Grayscale8)
@@ -327,7 +317,7 @@ class TestingGUI(QMainWindow):
         test_path = QFileDialog.getExistingDirectory(self, "Select test Folder Path")
         if test_path:
             self.args.test_path = test_path
-
+            # Update UI to show that the test folder path has been loaded
             self.config_path_label.setText(f"test Folder: {test_path}")
             print('test set')
             test_set = ReadDatasets(dataPath = self.args.test_path,
@@ -342,20 +332,20 @@ class TestingGUI(QMainWindow):
 
     def start_testing(self):
         if hasattr(self, 'args'):
-            self.start_testing_btn.setEnabled(False)
-            self.testing_thread = TestingThread(self)
-            self.testing_thread.finished.connect(self.on_testing_finished)
-            self.testing_thread.imageReady.connect(self.update_image)
-            self.testing_thread.start()
+            self.start_testing_btn.setEnabled(False)  # Disable the button to prevent multiple clicks
+            self.testing_thread = TestingThread(self)  # Create instance of testing thread
+            self.testing_thread.finished.connect(self.on_testing_finished)  # Connect finished signal to slot
+            self.testing_thread.imageReady.connect(self.update_image)  # Connect image ready signal to update image slot
+            self.testing_thread.start()  # Start the testing thread
         else:
             QMessageBox.warning(self, "Configuration Required",
                                 "Please load a configuration file before starting the testing.")
 
     def stop_testing(self):
         if hasattr(self, 'testing_thread'):
-            self.testing_thread.stop()
-            self.start_testing_btn.setEnabled(True)
-            self.stop_testing_btn.setEnabled(True)
+            self.testing_thread.stop()  # Call stop method of testing thread
+            self.start_testing_btn.setEnabled(True)  # Re-enable start button after stopping
+            self.stop_testing_btn.setEnabled(True)  # Enable stop button when stopping
 
     def on_testing_finished(self):
         self.start_testing_btn.setEnabled(True)  # Re-enable the button after testing is finished
